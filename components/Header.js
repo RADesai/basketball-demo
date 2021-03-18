8f9adb0bde52a53dff1f7854c93c3fd707d36780
Raj Ashish Desai <rajdesai94@gmail.com>
2021-03-17 21:02:12 -0400

    feat(trade-setup): setup for trade

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div className="bg-gradient-to-t from-white via-purple-50 to-purple-200 pb-5">
+        <div className="bg-gradient-to-b from-white via-purple-50 to-purple-50 pb-5">
             NBA TRADE DEMO
         </div>
     )
--- components/tradeSetup.js
+++ components/tradeSetup.js
@@ -13,33 +13,25 @@ function TradeSetup({ players, teams }) {
     const teamsSet = left && right && (left.tid !== right.tid);
     return (
         <div className="text-purple-800 text-center">
-            <div className="min-h-20">
-            {
-                teamsSet && <div className="grid grid-cols-7 p-2">
-                    <div className="col-span-3 text-2xl font-light pt-1">
-                        {left.city} {left.name}
-                    </div>
-                    <div className="col-span-1">
-                        <Image src="/assets/bounce.png" alt="scroll-down-icon" height="64" width="64" className="" />
-                    </div>
-                    <div className="col-span-3 text-2xl font-light pt-1">
-                        {right.city} {right.name}
-                    </div>
-                </div>
-            }
-            </div>
-            <div className="grid xs:grid-cols-1 sm:grid-cols-2 font-light bg-purple-50 p-10">
-                <div className="col-span-1 text-lg align-middle px-2 py-8 text-purple-900 mx-auto">
+            <div className="grid place-items-center xs:grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 font-light bg-purple-50 p-10 shadow-xl">
+                <div className="xs:col-span-1 lg:col-span-2 text-lg text-purple-900">
+                    <img src={left.logo} className="mx-auto"/>
                     <select className="border rounded-md p-1 border-purple-400 bg-purple-50 font-bold" onChange={(e) => setLeft(findTeam(teams, e.target.value))} placeholder="Select Team 1">
-                        { teams.map((team) => <option value={team.tid} key={team.tid}>{team.name}</option>) }
+                        <option value="" disabled selected>Select Team 1</option>
+                        { teams.map((team) => <option value={team.tid} key={team.tid}>{team.city} {team.name}</option>) }
                     </select>
-                    <img src={left.logo} />
                 </div>
-                <div className="col-span-1 text-lg align-middle px-2 py-8 text-purple-900 mx-auto">
+                <div className="col-span-1 my-10">
+                { teamsSet &&
+                    <Image src="/assets/bounce.png" alt="scroll-down-icon" height="64" width="64" />
+                }
+                </div>
+                <div className="xs:col-span-1 lg:col-span-2 text-lg text-purple-900">
+                    <img src={right.logo} className="mx-auto"  />
                     <select className="border rounded-md p-1 border-purple-400 bg-purple-50 font-bold" onChange={(e) => setRight(findTeam(teams, e.target.value))} placeholder="Select Team 2">
-                        { teams.map((team) => <option value={team.tid} key={team.tid}>{team.name}</option>) }
+                        <option value="" disabled selected>Select Team 2</option>
+                        { teams.map((team) => <option value={team.tid} key={team.tid}>{team.city} {team.name}</option>) }
                     </select>
-                    <img src={right.logo} />
                 </div>
             </div>
             <div className="mt-10">
@@ -51,7 +43,7 @@ function TradeSetup({ players, teams }) {
 
             }
                 <div>
-                    <Image src="/assets/logo.png" alt="NBA DEMO logo" height="256" width="256" />
+                    <Image src="/assets/logo.png" alt="NBA DEMO logo" height="256" width="256" className="hover:animate-bounce" />
                 </div>
             </div>
         </div>
