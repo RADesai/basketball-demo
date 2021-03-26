f30972c4c3fabe14fc1a1e5ab938ca109c79ed04
Raj Ashish Desai <rajdesai94@gmail.com>
2021-03-26 00:54:44 -0400

    chore(build): rename comps - 'git mv'

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Footer from './footer'
 const Layout = props => {
     return (
         <div className="text-center text-purple-800 pb-40">
-            <Header isFetching={props.playerQuery?.isFetching()} />
+            <Header isFetching={props.query.isFetching()} />
             { React.cloneElement(props.children, props) }
             <Footer />
         </div>
